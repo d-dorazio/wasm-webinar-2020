d2989cf56d6d07f3495b2db4e4c5ce5366c0be2a
Daniele D'Orazio <d.dorazio96@gmail.com>
2020-08-31 15:19:49 +0200

    circle packing and a bit of styling

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// index.js
 import("../pkg/index.js")
     .catch(console.error)
     .then((w) => {
@@ -6,4 +5,21 @@ import("../pkg/index.js")
         console.log(w.fast_sort([4, 5, 7, 9, 20], "sorting"));
 
         document.getElementById("num-btn").onclick = w.increment_counter;
+
+        const canv = document.getElementById("packing-canvas");
+        const ctx = canv.getContext("2d");
+        canv.onclick = () => {
+            const width = ctx.canvas.width;
+            const height = ctx.canvas.height;
+            const circles = w.pack_circles(width, height, 0.8, 5.0, 5.0);
+
+            ctx.clearRect(0, 0, width, height);
+
+            ctx.fillStyle = "#ebb43b";
+            for (const c of circles) {
+                ctx.beginPath();
+                ctx.arc(c.x, c.y, c.r, 0.0, 2.0 * Math.PI);
+                ctx.fill();
+            }
+        };
     });
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
-use std::sync::atomic::{AtomicU32, Ordering};
+use std::{
+    f32::consts::PI,
+    sync::atomic::{AtomicU32, Ordering},
+};
 
+use rand::prelude::*;
+use serde::Serialize;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -26,21 +31,17 @@ pub fn main_js() -> Result<(), JsValue> {
     Ok(())
 }
 
-
-// lib.rs
 #[wasm_bindgen]
 pub fn fast_add(a: u32, b: u32) -> u32 {
     a + b
 }
 
-// lib.rs
 #[wasm_bindgen]
 pub fn fast_sort(mut v: Vec<u8>, msg: &str) -> Vec<u8> {
     console::log_2(&JsValue::from_str("msg:"), &JsValue::from_str(msg));
     v.sort();
     v
 }
-
 
 static COUNTER: AtomicU32 = AtomicU32::new(0);
 
@@ -49,7 +50,70 @@ pub fn increment_counter() {
     let prev_val = COUNTER.fetch_add(1, Ordering::SeqCst);
 
     let document = web_sys::window().unwrap().document().unwrap();
-    let num_div = document.get_element_by_id("num").expect("num div not found");
+    let num_div = document
+        .get_element_by_id("num")
+        .expect("num div not found");
 
     num_div.set_inner_html(&format!("counter: {}", prev_val + 1));
+}
+
+#[derive(Serialize)]
+pub struct Circle {
+    pub x: f32,
+    pub y: f32,
+    pub r: f32,
+}
+
+#[wasm_bindgen]
+pub fn pack_circles(width: f32, height: f32, coverage: f32, minr: f32, padding: f32) -> JsValue {
+    let circles = pack_circles_impl(width, height, coverage, minr, padding);
+    JsValue::from_serde(&circles).unwrap()
+}
+
+fn pack_circles_impl(
+    width: f32,
+    height: f32,
+    coverage: f32,
+    minr: f32,
+    padding: f32,
+) -> Vec<Circle> {
+    let mut circles: Vec<Circle> = vec![];
+
+    let mut area = 0.0;
+    let targert_area = width * height * coverage;
+
+    let mut stall_i = 0;
+
+    let mut rng = thread_rng();
+    while area < targert_area {
+        let x = rng.gen_range(0.0, 1.0) * width;
+        let y = rng.gen_range(0.0, 1.0) * height;
+        let mut r = f32::min(x.min(width - x), y.min(height - y)) - padding;
+
+        if r >= minr {
+            for cc in &circles {
+                let d2 = (x - cc.x).powi(2) + (y - cc.y).powi(2);
+                r = r.min(d2.sqrt() - cc.r - padding);
+
+                if r < minr {
+                    break;
+                }
+            }
+        }
+
+        if r < minr {
+            stall_i += 1;
+            if stall_i >= 1_000 {
+                break;
+            }
+            continue;
+        }
+
+        stall_i = 0;
+
+        circles.push(Circle { x, y, r });
+        area += PI * r.powi(2);
+    }
+
+    circles
 }
